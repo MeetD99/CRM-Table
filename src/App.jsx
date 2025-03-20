67ee3b0514f763021597cf2cba67833fbb74028a
MeetD99 <meetdholakia2074@gmail.com>
2025-03-21 00:45:02 +0530

    github and docs link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Table from './Table'
-import './App.css'
 
 const App = () => {
   return (
     <div>
-      <h1>Table</h1>
+      <h1>CRM/ERP Table</h1>
+      <a href="https://github.com/MeetD99/CRM-Table" target='_blank'><button style={{
+        marginBottom: '30px',
+        padding: '10px',
+        backgroundColor: 'rgba(86, 27, 162, 1)',
+        color: 'white',
+        cursor: 'pointer',
+        borderRadius: '50px'
+      }}>Github and Docs!</button></a>
       <Table />
     </div>
   )
